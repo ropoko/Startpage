6458673ad9ab2c52f09aa1dcf7db69d330449af5
ropoko <rodrigostramantinoli@gmail.com>
2021-06-08 19:54:36 -0300

    added getTheme and getService to loader

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,34 @@
-    window.onload = () => {
-      document.getElementById("input_search").focus();
+window.onload = () => {
+  document.getElementById("input_search").focus();
 
-      document.getElementById("clock").innerHTML = getTime();
-      document.getElementById("message").innerText = defineMessage();
+  document.getElementById("clock").innerHTML = getTime();
+  document.getElementById("message").innerText = defineMessage();
 
-      // Set clock interval to tick clock
-      setInterval(() => {
-        document.getElementById("clock").innerHTML = getTime();
-      }, 30000);
-    };
+  // Set clock interval to tick clock
+  setInterval(() => {
+    document.getElementById("clock").innerHTML = getTime();
+  }, 30000);
+
+  getService();
+  getTheme();
+};
+
+function getService() {
+  console.log('hi')
+  let search_saved = localStorage.getItem('search-service');
+  document.getElementById('search_select').value = search_saved;
+
+  console.log(search_saved)
+}
+
+function getTheme() {
+  const currentTheme = localStorage.getItem('theme');
+
+  if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+      toggleSwitch.checked = true;
+    }
+  }
+}
